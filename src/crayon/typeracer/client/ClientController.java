5f017f8134542373561373de823b5af7dd3de512
vrizaldi <vrizaldi@gmail.com>
2019-07-14 22:52:21 +0700

    add pointer for client's snail

--- src/crayon/typeracer/client/ClientController.java
+++ src/crayon/typeracer/client/ClientController.java
@@ -50,6 +50,7 @@ public class ClientController extends FXController {
     private Text typed;
     private Text untyped;
     private Pane raceVisual;
+    private ImageView pointer;
 
     private boolean listening;
     private int id;
@@ -212,7 +213,8 @@ public class ClientController extends FXController {
                 System.out.println("parsing " + args[i]);
                 Player newPlayer = new Player();
                 players.put(Integer.valueOf(args[i]), newPlayer);
-                raceVisual.getChildren().add(newPlayer.initSnail(0.5 * DISTANCE + (i - 1) * DISTANCE));
+                ImageView p = Integer.parseInt(args[i]) == this.id ? pointer : null;
+                raceVisual.getChildren().add(newPlayer.initSnail(0.5 * DISTANCE + (i - 1) * DISTANCE, p));
 
             }
 
@@ -252,6 +254,10 @@ public class ClientController extends FXController {
             raceVisual.setMaxHeight(RACE_VISUAL_HEIGHT);
             raceVisual.setPrefHeight(RACE_VISUAL_HEIGHT);
             gamescreen.getChildren().add(raceVisual);
+
+            pointer = new ImageView(new Image("file:res/pointer.gif"));
+            pointer.setScaleX(0.2); pointer.setScaleY(0.2);
+            raceVisual.getChildren().add(pointer);
 
             this.sceneController.getScene().setOnKeyTyped(new EventHandler<KeyEvent>() {
                 @Override
--- src/crayon/typeracer/client/Player.java
+++ src/crayon/typeracer/client/Player.java
@@ -7,13 +7,16 @@ import javafx.scene.image.ImageView;
 public class Player {
     private int progress;
     private ImageView snail;
+    private ImageView pointer;
 
-    public ImageView initSnail(double posY) {
+    public ImageView initSnail(double posY, ImageView pointer) {
         System.out.println("initialising snail at " + posY);
         snail = new ImageView(new Image("file:res/snail.gif"));
         snail.setScaleX(-0.5);
         snail.setScaleY(0.5);
         snail.relocate(0, posY);
+        this.pointer = pointer;
+        if(pointer != null) pointer.relocate(0, posY - 50);
         return snail;
     }
 
@@ -21,6 +24,7 @@ public class Player {
         this.progress = progress;
 
         snail.setX(posX);
+        if(pointer != null) pointer.setX(posX);
     }
 
     public int getProgress() {
